a211f19187c7715f5ec2e6f59c53992a6bd1c424
Anwar Montasir <anwarmontasir@gmail.com>
2019-05-06 22:57:18 -0700

    added snapshot tests

---
+++ src/List.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import List from './List';
+
+describe('List component', () => {
+    it('renders the UI as expected', () => {
+        const tree = renderer
+        .create(<List key='1' header='First list' cards={[]} />)
+        .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
